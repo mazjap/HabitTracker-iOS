1541ba5ce817963a7ebe18168b15a273a5469a6a
Joshua Sharp <sharp.josh@gmail.com>
2019-11-15 19:32:20 -0500

    Base project created and organized

---
+++ Habit Tracker/Habit Tracker/View Controllers/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Habit Tracker
+//
+//  Created by Lambda_School_Loaner_214 on 11/15/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
