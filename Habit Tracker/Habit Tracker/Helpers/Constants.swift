86f2c2417856b83d7be7442a63f1fe4e3aa9732e
Joshua Sharp <sharp.josh@gmail.com>
2019-11-18 15:20:09 -0500

    Added CoreDateStack and constants

---
+++ Habit Tracker/Habit Tracker/Helpers/Constants.swift
@@ -0,0 +1,17 @@
+//
+//  Constants.swift
+//  Journal
+//
+//  Created by Joshua Sharp on 9/19/19.
+//  Copyright © 2019 Lambda. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+typealias CompletionWithError = (_ error: Error?) -> Void
+
+let coreDataModelName: String   = "Habit_Tracker"
+let debuging: Bool              = true
+
+//let cornerRadius:CGFloat        = 10
