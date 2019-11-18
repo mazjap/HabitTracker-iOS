afde0f29b070eee2391fc7324f9a6c1077fbfbf1
Jordan Christensen <jordan.c4922@gmail.com>
2019-11-19 06:04:52 +0800

    Set up calender storyboard and view controller

---
+++ Habit Tracker/Habit Tracker/Views/DateCell.swift
@@ -0,0 +1,14 @@
+//
+//  DateCell.swift
+//  Habit Tracker
+//
+//  Created by Jordan Christensen on 11/19/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import JTAppleCalendar
+import UIKit
+
+class DateCell: JTACDayCell {
+    @IBOutlet var dateLabel: UILabel!
+}
