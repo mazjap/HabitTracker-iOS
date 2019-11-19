e3506980cd9d949dbd4c4ff2b310a59a3fe0355a
Jordan Christensen <jordan.c4922@gmail.com>
2019-11-20 02:15:47 +0800

    Make calendar horizontally scroll, make dates selectable, add chart view

---
+++ Habit Tracker/Habit Tracker/Helpers/LocalNotificationManager.swift
@@ -0,0 +1,9 @@
+//
+//  LocalNotificationManager.swift
+//  Habit Tracker
+//
+//  Created by Jordan Christensen on 11/20/19.
+//  Copyright © 2019 Lambda School. All rights reserved.
+//
+
+import Foundation
--- Habit Tracker/Habit Tracker/View Controllers/CalenderViewController.swift
+++ Habit Tracker/Habit Tracker/View Controllers/CalenderViewController.swift
@@ -10,12 +10,21 @@ import UIKit
 import JTAppleCalendar
 
 class CalenderViewController: UIViewController {
+    
+    let formatter = DateFormatter()
 
     @IBOutlet weak var habitMonthView: JTACMonthView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        habitMonthView.scrollingMode = .stopAtEachCalendarFrame
+        habitMonthView.scrollDirection = .horizontal
+        habitMonthView.showsHorizontalScrollIndicator = false
+    }
+    
+    func updateViews() {
+        habitMonthView.reloadData()
     }
     
 
@@ -33,7 +42,6 @@ class CalenderViewController: UIViewController {
 
 extension CalenderViewController: JTACMonthViewDataSource, JTACMonthViewDelegate {
     func configureCalendar(_ calendar: JTACMonthView) -> ConfigurationParameters {
-        let formatter = DateFormatter()
         formatter.dateFormat = "yyyy MM dd"
         let currentDate = Date()
         let difference = TimeInterval(exactly: 15552000)!
@@ -42,21 +50,56 @@ extension CalenderViewController: JTACMonthViewDataSource, JTACMonthViewDelegate
         return ConfigurationParameters(startDate: startDate, endDate: endDate)
     }
     func calendar(_ calendar: JTACMonthView, didSelectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) {
-        
+        configureCell(view: cell, cellState: cellState)
     }
 
     func calendar(_ calendar: JTACMonthView, didDeselectDate date: Date, cell: JTACDayCell?, cellState: CellState, indexPath: IndexPath) {
-        
+        configureCell(view: cell, cellState: cellState)
     }
     
     func calendar(_ calendar: JTACMonthView, cellForItemAt date: Date, cellState: CellState, indexPath: IndexPath) -> JTACDayCell {
         let cell = calendar.dequeueReusableJTAppleCell(withReuseIdentifier: "dateCell", for: indexPath) as! DateCell
-        cell.dateLabel.text = cellState.text
+        self.calendar(calendar, willDisplay: cell, forItemAt: date, cellState: cellState, indexPath: indexPath)
         return cell
     }
     
     func calendar(_ calendar: JTACMonthView, willDisplay cell: JTACDayCell, forItemAt date: Date, cellState: CellState, indexPath: IndexPath) {
-        let cell = cell as! DateCell
+        configureCell(view: cell, cellState: cellState)
+    }
+    
+    func configureCell(view: JTACDayCell?, cellState: CellState) {
+        guard let cell = view as? DateCell  else { return }
         cell.dateLabel.text = cellState.text
+        handleCellTextColor(cell: cell, cellState: cellState)
+        handleCellSelected(cell: cell, cellState: cellState)
+    }
+        
+    func handleCellTextColor(cell: DateCell, cellState: CellState) {
+        if cellState.dateBelongsTo == .thisMonth {
+            cell.dateLabel.textColor = UIColor.black
+        } else {
+            cell.dateLabel.textColor = UIColor.gray
+        }
+    }
+    
+    func handleCellSelected(cell: DateCell, cellState: CellState) {
+        if cellState.isSelected {
+            cell.selectedView.layer.cornerRadius =  13
+            cell.selectedView.isHidden = false
+        } else {
+            cell.selectedView.isHidden = true
+        }
+    }
+    
+    func calendar(_ calendar: JTACMonthView, headerViewForDateRange range: (start: Date, end: Date), at indexPath: IndexPath) -> JTACMonthReusableView {
+        formatter.dateFormat = "MMMM"
+        
+        let header = calendar.dequeueReusableJTAppleSupplementaryView(withReuseIdentifier: "DateHeader", for: indexPath) as! DateHeader
+        header.monthLabel.text = formatter.string(from: range.start)
+        return header
+    }
+
+    func calendarSizeForMonths(_ calendar: JTACMonthView?) -> MonthSize? {
+        return MonthSize(defaultSize: 50)
     }
 }
